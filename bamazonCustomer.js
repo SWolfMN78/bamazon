4a7feb6381521ae4063e613064f774dd07514424
Wolfknightx <Wolfknightx@Gmail.com>
2018-02-20 17:28:10 -0600

    started into the project, scripting out the program.

---
+++ bamazonCustomer.js
@@ -0,0 +1,42 @@
+//requre mysql to make use of it for the application.
+var request = require("request");
+var mySQL = require("mysql");
+require("dotenv").config();
+
+//stashing the connection informatin in variables for easy & fast adjustment later.
+var dbHost = "localhost";
+var dbPort = 3306;
+var dbUser = "root";
+var dbPassword = process.env.MYSQL_PASSWORD;
+var dbDatabase = "bamazondb";
+
+//connect to the DB and press the information up to the database.
+var connection = mySQL.createConnection({
+    host: dbHost,
+    port: dbPort,
+    user: dbUser,
+    password: dbPassword,
+    database: dbDatabase,
+});
+
+//throw an error if the DBA doesn't exist.
+connection.connect(function(error) {
+    if (error) throw error;
+
+    console.log("connected as ID " + connection.threadId);
+    afterConnection();
+});
+
+/* Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+The app should then prompt users with two messages.
+
+The first should ask them the ID of the product they would like to buy.
+The second message should ask how many units of the product they would like to buy. */
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(error, result) {
+        if (error) throw error;
+
+        console.log("first record: ", result[0].product_name);
+    });
+}
